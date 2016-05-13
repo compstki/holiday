7080acfa44cb5e0611f007689d8fdd0048687dcf
Brendan McCart <Guest@stk-cs01-ws20.westlothian.sch.uk>
2016-05-13 17:56:58 +0100

    sp0.1

--- HOLIDAY.java
+++ HOLIDAY.java
@@ -1,13 +1,20 @@
 
-
 public class HOLIDAY
 {
 
+    private String name;
+    private int nights;
 
     public HOLIDAY()
     {
+        name = "";
+        nights = 0;
 
-
+    }
+    
+    public void enterName()
+    {
+        name = enterText("Please enter the name");
     }
 
 }
