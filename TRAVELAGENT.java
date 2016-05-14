25047ee1730c4dbb4666b6284f0df12de7ce379a
comptski <brendan.mccart@stkentigernsacademy.org.uk>
2016-05-14 01:48:44 +0100

    sprint3

--- TRAVELAGENT.java
+++ TRAVELAGENT.java
@@ -1,8 +1,12 @@
+import java.util.*;
 
 public class TRAVELAGENT
 {
 
-    private HOLIDAY holiday;
+    //private HOLIDAY holiday;
+    private HOLIDAY[] holidayList;
+    private Random random = new Random();
+    int winner = random.nextInt(5);
 
     public TRAVELAGENT()
     {
@@ -11,14 +15,30 @@ public class TRAVELAGENT
 
     public void setUpHolidays()
     {
-        holiday = new HOLIDAY();
+        //holiday = new HOLIDAY();
+        holidayList = new HOLIDAY[5];
+        for (int i = 0; i<5 ; i++)
+        {
+            holidayList[i] = new HOLIDAY();
+        }
     }
 
     public void processHoliday()
     {
-        holiday.enterName();
-        holiday.enterNights();
-        holiday.calculateTotalCost();
-        holiday.displayResult();
+        for (int i = 0; i<5 ; i++)
+        {
+            holidayList[i].enterName();
+            holidayList[i].enterNights();
+            holidayList[i].calculateTotalCost();
+            holidayList[i].displayResult();
+            if (i == winner)
+            {
+                System.out.println("yes");
+            }
+            else
+            {
+                System.out.println("no");
+            }
+        }
     }
 }
